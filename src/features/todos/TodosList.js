92d7605354937d6b92056c581fc80a8cd5c80e4b
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-06-14 17:29:06 +0300

    Full CRUD with RTK

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { useGetTodosQuery } from '../api/apiSlice';
+import {
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+} from '../api/apiSlice';
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState('');
@@ -14,9 +19,13 @@ const TodoList = () => {
     error,
   } = useGetTodosQuery();
 
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    addTodo({ userId: 1, title: newTodo, completed: false });
     setNewTodo('');
   };
 
@@ -40,7 +49,25 @@ const TodoList = () => {
 
   let content;
   if (isLoading) content = <p>Loading...</p>;
-  else if (isSuccess) content = JSON.stringify(todos);
+  else if (isSuccess)
+    content = todos.map((todo) => (
+      <article key={todo.id}>
+        <div className="todo">
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            id={todo.id}
+            onChange={() => {
+              updateTodo({ ...todo, completed: !todo.completed });
+            }}
+          />
+          <label htmlFor={todo.id}>{todo.title}</label>
+        </div>
+        <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </article>
+    ));
   else if (isError) <p>{error}</p>;
 
   return (
